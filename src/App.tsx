fa3e53aa9b26641209fca94550c4b75aeda248d2
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-05-30 12:11:40 +0800

    refactor: replace placeholder feed with new feed page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AppShell } from './components/layout/AppShell';
-import Feed from './pages/Feed';
+import { ProtectedRoute } from './components/layout/ProtectedRoute';
+import FeedPage from './pages/FeedPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppShell />}>
-          <Route index element={<Feed />} />
+        <Route path="/login" element={<LoginPage />} />
+        
+        <Route path="/" element={
+          <ProtectedRoute>
+            <AppShell />
+          </ProtectedRoute>
+        }>
+          <Route index element={<FeedPage />} />
           <Route path="watchlist" element={<div className="p-4">Watchlist Placeholder</div>} />
           <Route path="events" element={<div className="p-4">Events Placeholder</div>} />
           <Route path="birthdays" element={<div className="p-4">Birthdays Placeholder</div>} />
