a1d7788f0d42231bd6a469327d21bab64a92b928
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-05-30 12:11:40 +0800

    fix: correct tailwind config syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,26 @@ export default {
         sm: 'var(--shadow-sm)',
         md: 'var(--shadow-md)',
         lg: 'var(--shadow-lg)',
-      }
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
