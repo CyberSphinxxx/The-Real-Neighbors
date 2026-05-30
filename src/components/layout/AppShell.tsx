69b09222058aeb68d4bdd06887d9ae7354c4bc48
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-05-30 15:18:44 +0800

    update AppShell.tsx

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -5,6 +5,7 @@ import { TopBar } from './TopBar';
 import { MobileNav } from './MobileNav';
 import { BirthdayWidget } from '../birthdays/BirthdayWidget';
 import { EventWidget } from '../events/EventWidget';
+import { ErrorBoundary } from '../ui/ErrorBoundary';
 
 export const AppShell: React.FC = () => {
   return (
@@ -22,7 +23,11 @@ export const AppShell: React.FC = () => {
       {/* Center Content */}
       <main className="flex-1 overflow-y-auto w-full max-w-full">
         <div className="mx-auto max-w-3xl p-4 md:p-6 w-full pb-24 md:pb-6">
-          <Outlet />
+          <ErrorBoundary>
+            <div className="animate-in fade-in duration-300">
+              <Outlet />
+            </div>
+          </ErrorBoundary>
         </div>
       </main>
 
