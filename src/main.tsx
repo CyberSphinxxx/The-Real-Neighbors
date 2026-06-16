3ac8d9a888500cae1f98089a1f874b7bccca8d93
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-06-16 22:56:15 +0800

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ConfirmProvider } from './contexts/ConfirmContext';
+import { ErrorBoundary } from './components/ui/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfirmProvider>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </ConfirmProvider>
   </StrictMode>,
 )
