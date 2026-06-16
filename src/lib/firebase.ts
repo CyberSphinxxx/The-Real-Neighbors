6e1d3b193b9f8f5bca337fb394777c47c2d2d11e
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-06-16 22:56:14 +0800

    Update src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || 'https://the-real-neighbors-default-rtdb.asia-southeast1.firebasedatabase.app'
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 export const app = initializeApp(firebaseConfig);
